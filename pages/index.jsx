2efc648466159cbaeff1ca2112ac97be90f0bc8e
Prince Codes <princecodes247@gmail.com>
2022-06-15 16:18:18 -0400

    integrate ethers

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 
-import { useState } from "react";
+let provider;
 
 const TransactionBox = (props) => {
   return (
@@ -17,13 +19,53 @@ const TransactionBox = (props) => {
 };
 
 export default function Home() {
+  const [account, setAccount] = useState("");
+  useEffect(() => {
+    async function load() {
+      provider = new ethers.providers.Web3Provider(window.ethereum);
+
+      // MetaMask requires requesting permission to connect users accounts
+      let accounts = await provider.send("eth_requestAccounts", []);
+      // console.log(ress);
+      // const accounts = await web3.eth.requestAccounts();
+
+      setAccount(accounts[0]);
+    }
+
+    load();
+  }, []);
+
+  let waitingCount = 0;
   const [isTransactionOpen, setIsTransactionOpen] = useState(false);
   const [transaction, setTransaction] = useState({
+    _id: "",
     address: "0x0",
     amount: "0",
     name: "Prince",
     timeout: "0",
   });
+
+  async function waitForBalance(account, amount, cb, limit = 10, first = true) {
+    waitingCount = first == true ? 0 : waitingCount + 1;
+
+    let balance = await provider.getBalance(account);
+    console.log(balance, amount, limit, waitingCount, first == true);
+    if (balance >= amount) {
+      cb();
+    } else if (waitingCount < limit) {
+      // Try again in 1 second.
+      let timer = setTimeout(function () {
+        waitForBalance(account, amount, cb, limit, false);
+      }, 5000);
+      return () => clearTimeout(timer);
+    } else {
+      console.log("Transaction failed");
+    }
+  }
+
+  // waitForBalance("0x123abc987...", web3.toWei(1, "ether"), function () {
+  //   alert("1 ether received!");
+  // });
 
   const showTransactionBox = () => {
     // const transactionBox = document.querySelector(".transaction-box");
@@ -32,20 +74,39 @@ export default function Home() {
   const [name, setName] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetch("/api/transaction", {
+    fetch("http://localhost:2020/api/transactions", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
         amount,
-        name,
+        from: name,
+        to: "prince",
       }),
     })
       .then((res) => res.json())
+      .then((data) => data.data)
       .then((data) => {
-        setTransaction(data);
+        console.log(data);
+        const newTransaction = {
+          _id: data._id,
+          address: data.ethAddress,
+          amount: data.amount,
+          name: data.from,
+          timeout: data.timeout,
+          to: data.to,
+        };
+        setTransaction(newTransaction);
         setIsTransactionOpen(true);
+        waitForBalance(
+          newTransaction.address,
+          ethers.utils.parseEther(newTransaction.amount.toString()),
+          function () {
+            alert("1 ether received!");
+          },
+          10
+        );
       })
       .catch((err) => {
         console.log(err);
@@ -62,7 +123,7 @@ export default function Home() {
     <div>
       <main className="h-screen flex items-center justify-center">
         <h1>Make Payment to @prince</h1>
-
+        {account}
         <form onSubmit={handleSubmit} action="">
           <label htmlFor="amount">Amount</label>
           <input
